45ca8a9646bfaf1fcf12d12e323d0fd1829aceed
Penny Tague <pmtague9@gmail.com>
2019-07-12 08:26:21 -0700

    Created header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headerContainer = document.querySelector('.headerContainer');
+
+headerContainer.appendChild(header());
+
+function header() {
+
+    // Create Elements
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temperature = document.createElement('span');
+
+    // Set Structure
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temperature);
+
+    // Set Class Names
+    header.classList.add('header');
+    date.classList.add('date');
+    temperature.classList.add('temp');
+
+    return header;
+    
+}
